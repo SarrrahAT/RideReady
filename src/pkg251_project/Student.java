fac29c905b3df9879288832e02aa49862823128d
Sarah_Altalhi <sara13altalhi@gmail.com>
2024-04-16 21:04:42 +0300

    RideReady_251Project

---
+++ src/pkg251_project/Student.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pkg251_project;
+import java.util.Date;
+public class Student {
+   private String studentName;
+    private String phoneNum;
+    private String email;
+    private String homeAddress;
+    private String username;
+    private String password;
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public String getPhoneNum() {
+        return phoneNum;
+    }
+
+    public void setPhoneNum(String phoneNum) {
+        this.phoneNum = phoneNum;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getHomeAddress() {
+        return homeAddress;
+    }
+
+    public void setHomeAddress(String homeAddress) {
+        this.homeAddress = homeAddress;
+    } 
+    
+}
