17f14efd2bff27416c1678e36da6a2d5db5af16d
Sarah_Altalhi <sara13altalhi@gmail.com>
2024-04-17 19:19:11 +0300

    RideReady_251Project

---
+++ src/Subscription.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.util.Date;
+  // created by Raghad Farghal-2206935
+/**
+ *
+ * @author raghadfarghal
+ */
+public class Subscription {
+    private Date startDate;
+    private Date endDate;
+    private boolean isActive;
+    private int monthNum;
+    private int cost;
+    private Student student;
+
+    // Standard getters and setters for each field
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public boolean isIsActive() {
+        return isActive;
+    }
+
+    public void setIsActive(boolean isActive) {
+        this.isActive = isActive;
+    }
+
+    public int getMonthNum() {
+        return monthNum;
+    }
+
+    public void setMonthNum(int monthNum) {
+        this.monthNum = monthNum;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    public void setCost(int cost) {
+        this.cost = cost;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+// Method for making a subscription
+    
+    
+}
+
